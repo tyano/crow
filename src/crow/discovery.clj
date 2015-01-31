02dba921bd48c19fdda79327b411230fdf3c6869
Tsutomu YANO <t_yano@me.com>
2015-01-31 17:32:06 +0900

    implemented registrar-handling fns for discovery.

--- src/crow/discovery.clj
+++ src/crow/discovery.clj
@@ -1,8 +1,33 @@
 (ns crow.discovery
   (:require [crow.protocol :refer [discovery]]
             [crow.registrar-source :as source]
-            [crow.service :as service]))
+            [crow.service :as service]
+            [clojure.set :refer [difference]]))
 
+(def active-registrars (ref #{}))
+(def dead-registrars (ref #{}))
+
+(defn- reset-registrars!
+  [registrar-source]
+  (let [new-registrars (source/registrars registrar-source)]
+    (dosync
+      (io!
+        (alter active-registrars
+          (fn [_]
+            (difference new-registrars @dead-registrars)))))))
+
+(defn- next-registrar
+  []
+  (dosync
+    (first (shuffle @active-registrars))))
+
+(defn- abandon-registrar!
+  [reg]
+  (dosync
+    (io!
+      (let [reg (first (shuffle (alter active-registrars disj reg)))]
+        (alter dead-registrars conj reg)
+        reg))))
 
 (defn- discover-with
   [registrar service-name attribute]
@@ -10,5 +35,6 @@
 
 (defn discover
   [registrar-source service-name attribute]
-  (let [registrars (source/registrars registrar-source)]
-    ))
+  (when-not (seq @active-registrars)
+    (reset-registrars! registrar-source)))
+

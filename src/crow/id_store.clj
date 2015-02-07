8f6c11107361a14ebf37962672aa7d200fc2fdb6
Tsutomu YANO <t_yano@me.com>
2015-02-08 04:18:17 +0900

    moved ServiceIdStore into a new namespace.

---
+++ src/crow/id_store.clj
@@ -0,0 +1,30 @@
+(ns crow.id-store
+  (:refer-clojure :exclude [read])
+  (:require [clojure.string :refer [blank?]]
+            [clojure.java.io :refer [writer reader]])
+  (:import [java.io FileNotFoundException Writer Reader]))
+
+(defprotocol ServiceIdStore
+  (write [this service-id] "write service-id into persistent store.")
+  (read [this] "read service-id from persistent store."))
+
+(extend-protocol ServiceIdStore
+  nil
+  (write [this service-id] nil)
+  (read [this] nil))
+
+
+(defrecord FileIdStore [file-path]
+  ServiceIdStore
+  (write [this service-id]
+    (when (not (blank? service-id))
+      (with-open [w ^Writer (writer file-path :append false)]
+        (.write w service-id)
+        (.newLine w))))
+  (read [this]
+    (try
+      (with-open [r ^Reader (reader file-path)]
+        (.readLine r))
+      (catch FileNotFoundException ex
+        nil))))
+

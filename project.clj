0d666b39f12921fa0f590012d4c6e90cd39b02f0
Tsutomu YANO <t_yano@me.com>
2015-02-19 18:06:05 +0900

    changed version string.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crow "0.1.0-SNAPSHOT"
+(defproject crow "1.0-SNAPSHOT"
   :description "Crow is a library for collaborating distributed service implemented by Clojure."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"

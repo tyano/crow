631e66a2c2ef72581de3b37dcb3d30879f08e480
Tsutomu YANO <t_yano@me.com>
2015-06-02 02:53:50 +0900

    added crow.marshaller into aot.

--- project.clj
+++ project.clj
@@ -17,7 +17,11 @@
                                   [ch.qos.logback/logback-classic "1.1.2"]]
                    :resource-paths ["resources-dev"]
                    :jvm-opts ["-Djava.net.preferIPv4Stack=true"]}}
-  :aot [crow.registrar-source crow.registrar crow.protocol crow.discovery]
+  :aot [crow.registrar-source
+        crow.registrar
+        crow.protocol
+        crow.discovery
+        crow.marshaller]
   :main crow.registrar)
 
 (cemerick.pomegranate.aether/register-wagon-factory!

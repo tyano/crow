6c624413be9ebd310e585bc64c0d895a8a4efae2
Tsutomu YANO <t_yano@me.com>
2015-07-30 05:16:03 +0900

    version-up of libraries and clojure.

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [aleph "0.4.0-SNAPSHOT"]
-                 [clojure-msgpack "1.1.0"]
-                 [clj-time "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [aleph "0.4.1-SNAPSHOT"]
+                 [clojure-msgpack "1.1.1"]
+                 [clj-time "0.10.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [slingshot "0.12.1"]
+                 [slingshot "0.12.2"]
                  [com.shelf/messagepack-framedecoder "1.0-SNAPSHOT"]]
   :plugins [[lein-midje "3.1.3"]]
   :repositories {"javelindev-snapshots" "http://javelindev.jp/repository/snapshots"}
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [ch.qos.logback/logback-classic "1.1.2"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]
+                                  [ch.qos.logback/logback-classic "1.1.3"]]
                    :resource-paths ["resources-dev"]
                    :jvm-opts ["-Djava.net.preferIPv4Stack=true"]}}
   :aot [crow.registrar-source

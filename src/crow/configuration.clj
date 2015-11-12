2d571cfc370ea4e5f6f1605c0f247ac071bfed39
Tsutomu YANO <t_yano@me.com>
2015-11-12 21:02:27 +0900

    resolve all constructor symbols in config when loading time.
    All keys which might contains a symbol or string of fn name should
    be resolved (by executing the fn) before the use of config object.
    Because it is verbose if users need to check if the value of config
    could be a symbol, resolve it and execute it everytime when using
    a config object.

--- src/crow/configuration.clj
+++ src/crow/configuration.clj
@@ -13,6 +13,30 @@
   [config]
   (assert (map? config) "A configuration file must return a map containing service configurations."))
 
+(defn- exec-constructor
+  "convert a value to a function if the value is a symbol or a string for a function."
+  [sym config]
+  (if (or (symbol? sym) (string? sym))
+    (let [fn-sym (if (symbol? sym) sym (symbol sym))
+          fn-ns  (namespace fn-sym)]
+      (require (symbol fn-ns))
+      (if-let [resolved-fn (find-var fn-sym)]
+        (resolved-fn config)
+        (throw (IllegalStateException. (str "No such function: " sym)))))
+    sym))
+
+(def constructor-fn-keys #{:registrar-source :id-store :object-marshaller})
+
+(defn- resolve-constructor-fn
+  "resolve all keys in a configuration which might be a symbol or a string of
+  constructor-function. if a fn is resolved, the fn will be executed with 'config' as a argument."
+  [config]
+  (when config
+    (loop [c config ks constructor-fn-keys]
+      (if-let [k (first ks)]
+        (recur (update c k #(exec-constructor % config)) (rest ks))
+        c))))
+
 (defn from-path
   "Create a configuration-map from a source path.
   The file must be a readable clojure source file and must return a map for evaluation.
@@ -21,8 +45,7 @@
   [file-path]
   (let [config (read-from-file file-path)]
     (assert-map config)
-    config))
-
+    (resolve-constructor-fn config)))
 
 (defn from-edn
   "read a path containing EDN string.
@@ -33,5 +56,5 @@
   (with-open [stream (PushbackReader. (reader file-path))]
     (let [config (edn/read stream)]
       (assert-map config)
-      config)))
+      (resolve-constructor-fn config))))
 

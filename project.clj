9a50eda1cdf46ea1cc64bc624326d3feb04218d8
Tsutomu YANO <t_yano@me.com>
2016-05-15 18:20:54 +0900

    aleph 0.4.1 is valid. problems occurred for manifold 0.1.4. down to 0.1.3.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject crow "1.0-SNAPSHOT"
-  :description "Crow is a library for collaborating distributed service implemented by Clojure."
+  :description "Crow is a library for collaborating with distributed services implemented by Clojure."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [aleph "0.4.1-beta2"]
+                 [aleph "0.4.1" :exclusions [manifold]]
+                 [manifold "0.1.3"]
                  [clojure-msgpack "1.2.0"]
                  [clj-time "0.11.0"]
                  [org.clojure/core.async "0.2.374"]

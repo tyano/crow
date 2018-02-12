bf1f41848222726569c00d7d7f2d93fde879d88d
Tsutomu YANO <t_yano@me.com>
2018-02-12 21:14:02 +0900

    Bump up versions.

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [async-connect "0.2.0-SNAPSHOT"]
                  [clojure-msgpack "1.2.1"]
-                 [clj-time "0.14.0"]
-                 [org.clojure/core.async "0.3.443"]
+                 [clj-time "0.14.2"]
+                 [org.clojure/core.async "0.4.474"]
                  [org.clojure/tools.logging "0.4.0"]
                  [slingshot "0.12.2"]
                  [com.shelf/messagepack-framedecoder "1.0-SNAPSHOT"]
                  [ns-tracker "0.3.1"]
                  [clj-http "3.7.0"]
-                 [integrant "0.6.1"]
+                 [integrant "0.6.3"]
                  [org.clojure/test.check "0.9.0"]]
   :plugins [[lein-midje "3.2"]]
   :repositories [["Shelf Public Release Repository" {:url "https://shelf-maven-repo.s3-ap-northeast-1.amazonaws.com/release"
@@ -24,7 +24,7 @@
                                                       :update :always}]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :profiles {:dev {:dependencies [[midje "1.9.0-alpha6"]
+  :profiles {:dev {:dependencies [[midje "1.9.1"]
                                   [ch.qos.logback/logback-classic "1.2.3"]]
                    :resource-paths ["resources-dev"]
                    :jvm-opts ["-Djava.net.preferIPv4Stack=true"]}
@@ -38,5 +38,6 @@
         crow.discovery
         crow.marshaller
         crow.remote]
-  :main crow.registrar)
+  :main crow.registrar
+  :javac-options ["-source" "1.8" "-target" "1.8"])
 

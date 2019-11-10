14a0fe69ea24c3f7454b1c5b54dfbb9ae6f68416
Tsutomu YANO <t_yano@me.com>
2019-11-10 17:29:02 +0900

    async-connect 0.2.4

--- core/project.clj
+++ core/project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/tools.logging "0.5.0"]
                  [clj-http "3.10.0"]
                  [clj-time "_" :upgrade false]
-                 [async-connect "0.2.3-SNAPSHOT"]
+                 [async-connect "0.2.4-SNAPSHOT"]
                  [clojure-msgpack "1.2.1"]
                  [com.shelf/messagepack-framedecoder "1.0-SNAPSHOT"]]
   :plugins [[lein-modules "0.3.11"]]
